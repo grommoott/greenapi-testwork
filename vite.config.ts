ec203c869b1137a5b64d2bc6ba18bf3958f904fc
grommoott <grommoott@gmail.com>
2025-02-21 14:08:31 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
 import { defineConfig } from "vite"
+import path from "path"
 import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
+
+const dirname = __dirname || process.cwd()
 
 export default defineConfig({
     plugins: [react(), tailwindcss()],
     resolve: {
         alias: {
-            "@app": "src/1.App",
-            "@pages": "src/2.Pages",
-            "@widgets": "src/3.Widgets",
-            "@entities": "src/4.Entities",
-            "@shared": "src/5.Shared",
+            "@app": path.resolve(dirname, "./src/1.App/"),
+            "@pages": path.resolve(dirname, "./src/2.Pages/"),
+            "@widgets": path.resolve(dirname, "./src/3.Widgets/"),
+            "@entities": path.resolve(dirname, "./src/4.Entities/"),
+            "@shared": path.resolve(dirname, "./src/5.Shared/"),
         },
     },
 })
